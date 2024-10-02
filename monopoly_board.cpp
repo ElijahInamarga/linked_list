24ef67f2b59e5aef4b1d278aada48142adffa0a0
ElijahInamarga <elijahcek@gmail.com>
2024-10-01 22:51:52 -0700

    Finished optional functions and main() testing function

--- monopoly_board.cpp
+++ monopoly_board.cpp
@@ -226,7 +226,7 @@ public:
         delete temp;
     }
 
-    bool search(T value) {
+    bool search(T data) {
         if(headNode == nullptr) {
             cout << "Can't search empty list" <<endl;
             return false;
@@ -234,7 +234,7 @@ public:
 
         Node<T>* currentNode = headNode;
         do {
-            if(currentNode->data.isEqual(value)) {
+            if(currentNode->data.isEqual(data)) {
                 return true;
             }
             currentNode = currentNode->nextNode;
@@ -260,28 +260,72 @@ public:
     //Basic Funtions
     void reverseCLList() {
         cout << "Reverse List unwritten" << endl;
-    } void sortCLList() {
-        cout << "Sort List unwritten" << endl;
-    } void printHeadNode() {
-        cout << "Print Head Node unwritten" << endl;
-    } void printLastNode() {
-        cout << "Print Last Node unwritten" << endl;
-    } void isListEmpty() {
-        cout << "Is List Empty unwritten" << endl;
     }
-    void countNodes() {
-        cout << "Count Nodes unwritten" << endl;
+    void sortCLList() {
+        cout << "Sort List unwritten" << endl;
+    }
+    void printHeadNode() {
+        if(headNode == nullptr) {
+            cout << "Cant print the head node of an empty list" << endl;
+            return;
+        }
+        headNode->data.print();
+    }
+    void printLastNode() {
+        if(headNode == nullptr) {
+            cout << "Cant print the last node of an empty list" << endl;
+            return;
+        }
+        tailNode->data.print();
+    }
+
+    bool isListEmpty() {
+        if(headNode == nullptr) {
+           return true;
+        }
+        return false;
+    }
+
+    int countNodes() {
+        if(headNode == nullptr) {
+            return 0;
+        }
+
+        int count = 0;
+        Node<T>* currentNode = headNode;
+        do {
+            count++;
+            currentNode = currentNode->nextNode;
+        } while(currentNode != headNode);
+        return count;
     }
 
     //Optional Tasks
     // Advanced Functions
     void convertCLList() {
         cout << "Convert Circular List Unwritten." << endl;
-    } void updateNodeValue() {
-        cout << "update Node value unwritten" << endl;
-    } void displaySpecificColorNode() {
-        cout << "Display Specific color Node" << endl;
-    } void mergeCLList() {
+    }
+
+    void updateNodeValue(T value, T newValue) {
+        if(headNode == nullptr) {
+            cout << "Can't update a node of an empty list" <<endl;
+            return;
+        }
+
+        Node<T>* currentNode = headNode;
+        do {
+            if(currentNode->data.isEqual(value)) {
+                currentNode->data = newValue;
+            }
+            currentNode = currentNode->nextNode;
+        } while(currentNode != headNode);
+    }
+
+    void displaySpecificColorNode() {
+        cout << "Display Specific color Node Unwritten" << endl;
+    }
+
+    void mergeCLList() {
         cout << "Merge Circular Linked List Unwritten" << endl;
     }
 };
@@ -293,31 +337,57 @@ int main() {
     MonopolyBoard house("house", "white", 500, 100);
     MonopolyBoard apartment("apartment", "pink", 400, 50);
     MonopolyBoard hotel("hotel", "gray", 2000, 300);
-    MonopolyBoard park("park", "green", 1500, 200);
     MonopolyBoard pool("pool", "blue", 200, 15);
+    MonopolyBoard park("park", "green", 1500, 200);
 
     // Insert elements at the end
-    list.insertAtTail(house);
+    cout << "\n<========================================== MANDATORY FUNCTIONS =========================================>" << endl;
+    cout << "\nList after inserting MonopolyBoard(\"house\", \"white\", 500, 100) at head using insertAtHead():" << endl;
+    list.insertAtHead(house);
+    list.printList();
+    cout << "\nList after inserting MonopolyBoard(\"apartment\", \"pink\", 400, 50) at tail using insertAtTail():" << endl;
     list.insertAtTail(apartment);
-    list.insertAtTail(hotel);
+    list.printList();
+    cout << "\nList after inserting MonopolyBoard(\"hotel\", \"gray\", 2000, 300) at head using insertAtHead():" << endl;
+    list.insertAtHead(hotel);
+    list.printList();
+    cout << "\nList after inserting MonopolyBoard(\"park\", \"green\", 1500, 200) at tail using insertAtTail():" << endl;
     list.insertAtTail(park);
-    list.insertAtPosition(pool, 5);
-    list.deleteAtPosition(5);
+    list.printList();
+    cout << "\nList after inserting MonopolyBoard(\"pool\", \"blue\", 200, 15) at position 3 using insertAtPosition():" << endl;
+    list.insertAtPosition(pool, 3);
+    list.printList();
+    cout << "\nList after deleting node at head using deleteAtHead():" << endl;
     list.deleteAtHead();
     list.printList();
-    list.search(house) ? cout << "true" << endl : cout << "false" << endl;
-
-//    //Optional Basic Tasks
-//    list.reverseCLList();
-//    list.sortCLList();
-//    list.printHeadNode();
-//    list.printLastNode();
-//    list.isListEmpty();
-//    list.countNodes();
-//    //Optional Advanced Tasks
-//    list.convertCLList();
-//    list.updateNodeValue();
-//    list.displaySpecificColorNode();
-//    list.mergeCLList();
+    cout << "\nList after deleting node at tail using deleteAtTail():" << endl;
+    list.deleteAtTail();
+    list.printList();
+    cout << "\nList after deleting node at position 2 using deleteAtPosition():" << endl;
+    list.deleteAtPosition(2);
+    list.printList();
+    cout << "\n<======================================= OPTIONAL BASIC FUNCTIONS =======================================>" << endl;
+    cout << "\nPrint head of Monopoly Board list using printHeadNode():" << endl;
+    list.printHeadNode();
+    cout << "\nPrint last node of Monopoly Board list using printLastNode():" << endl;
+    list.printLastNode();
+    cout << "\nCheck if list is empty using isListEmpty():" << endl;
+    list.printList();
+    list.isListEmpty() ? cout << "List is empty" << endl : cout << "List is not empty" << endl;
+    cout << "\nFind out how many elements is in list using countNodes():" << endl;
+    cout << list.countNodes() << endl;
+    cout << "\n<====================================== OPTIONAL ADVANCED FUNCTIONS =====================================>" << endl;
+    cout << "\nUpdating data inside a certain node using updateNodeValue():" << endl;
+    list.updateNodeValue(house, pool);
+    list.printList();
+    cout << "\n<========================================== UNWRITTEN FUNCTIONS =========================================>" << endl;
+    //Optional Basic Tasks
+    cout << endl;
+    list.reverseCLList();
+    list.sortCLList();
+    //Optional Advanced Tasks
+    list.convertCLList();
+    list.displaySpecificColorNode();
+    list.mergeCLList();
     return 0;
 }
